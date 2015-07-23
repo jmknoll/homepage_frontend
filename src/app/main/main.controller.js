0a1bf36014eede9aa21e270c32e691c96fbdba1e
Jameson Knoll <j.knoll621@gmail.com>
2015-07-23 22:01:23 +0800

    build out landing page structure and contact form

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,34 +7,6 @@
 
       /** @ngInject */
       function MainController($timeout, webDevTec, toastr, $window, $scope) {
-        var vm = this;
-
-        vm.awesomeThings = [];
-        vm.classAnimation = '';
-        vm.creationDate = 1435475794607;
-        vm.showToastr = showToastr;
-
-        activate();
-
-        function activate() {
-          getWebDevTec();
-          $timeout(function() {
-            vm.classAnimation = 'rubberBand';
-          }, 4000);
-        }
-
-        function showToastr() {
-          toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-          vm.classAnimation = '';
-        }
-
-        function getWebDevTec() {
-          vm.awesomeThings = webDevTec.getTec();
-
-          angular.forEach(vm.awesomeThings, function(awesomeThing) {
-            awesomeThing.rank = Math.random();
-          });
-        }
 
         $scope.hideSubtext = true;
         $scope.hideTwitter = true;
@@ -48,6 +20,18 @@
           $timeout( function() { $scope.hideLinkedIn = false }, 2000 );
           $timeout( function() { $scope.hideGithub = false }, 2500 );
 
+        }
+
+        $scope.saveEmail = function() {
+          var contactInfo = {
+            'name': $scope.contact.name,
+            'email': $scope.contact.email,
+            'description': $scope.contact.description,
+            'price': $scope.contact.price
+          }
+
+          alert('Thank you for getting in touch ' + contactInfo.name + '. I will be in touch shortly to discuss this project further. Please confirm that your information below is correct.'
+            + JSON.stringify(contactInfo));
         }
       }
 
